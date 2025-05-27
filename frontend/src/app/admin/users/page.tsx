2fa9026e6f08fce949bec3e25aea40aa33ed79d9
Luis Faria <lfariabr@gmail.com>
2025-05-28 06:48:56 +1000

    feat(frontend-adjustments) adds stroke to admin page back

--- frontend/src/app/admin/users/page.tsx
+++ frontend/src/app/admin/users/page.tsx
@@ -192,6 +192,7 @@ export default function UsersAdminPage() {
                             height="24"
                             viewBox="0 0 24 24"
                             fill="none"
+                            stroke="currentColor"
                             strokeWidth="2"
                             strokeLinecap="round"
                             strokeLinejoin="round"
